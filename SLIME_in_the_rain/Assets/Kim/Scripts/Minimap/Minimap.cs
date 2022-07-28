29c12d81160b242afc647bbc243712525830db0e
MisungKims <90057774+MisungKims@users.noreply.github.com>
2022-07-28 19:47:14 +0900

    Update Minimap
    - 미니맵 범위 제한

--- SLIME_in_the_rain/Assets/Kim/Scripts/Minimap/Minimap.cs
+++ SLIME_in_the_rain/Assets/Kim/Scripts/Minimap/Minimap.cs
@@ -25,6 +25,7 @@ public class Minimap : MonoBehaviour
 
     [SerializeField]
     private GameObject slimeIconZoomIn;    // 축소 상태에서 맵 중간에 놓일 슬라임의 아이콘
+    private RectTransform slimeIconRect;
 
     [SerializeField]
     private MinimapIcon miniMapIconPrefab;         // 생성할 아이콘 프리팹
@@ -38,11 +39,19 @@ public class Minimap : MonoBehaviour
    [SerializeField]
     private float zoom = 2.3f;     // 줌인 할 배율
 
-    private MinimapWorldObject slimeObj;
-    private Vector3 slimePos;
-
     [SerializeField]
-    private Mask mask;
+    private float zoomInRange;
+    [SerializeField]
+    private float zoomOutRange;
+
+    // 슬라임이 미니맵의 범위를 벗어났는지?
+    private bool isOutRangeX;
+    private bool isOutRangeY;
+
+    private MinimapWorldObject slimeObj;
+    private GameObject slimeIconZoomOut;
+    private Vector2 slimePos;
+
    [SerializeField]
     private RectTransform maskTransform;
     [SerializeField]
@@ -73,6 +82,10 @@ public class Minimap : MonoBehaviour
             Destroy(this.gameObject);
         }
 
+        slimeIconRect = slimeIconZoomIn.GetComponent<RectTransform>();
+        slimeIconRect.anchoredPosition = Vector2.zero;
+        slimeIconRect.localScale *= zoom;
+
         ZoomIn();
     }
 
@@ -95,7 +108,7 @@ public class Minimap : MonoBehaviour
     {
         isZoomIn = true;
         slimeIconZoomIn.SetActive(true);
-        mask.enabled = true;
+        if(slimeIconZoomOut) slimeIconZoomOut.SetActive(false);
 
         maskTransform.anchoredPosition = zoomInTransform.anchoredPosition;
         maskTransform.sizeDelta = zoomInTransform.sizeDelta;
@@ -107,7 +120,7 @@ public class Minimap : MonoBehaviour
     {
         isZoomIn = false;
         slimeIconZoomIn.SetActive(false);
-        mask.enabled = false;
+        if(slimeIconZoomOut) slimeIconZoomOut.SetActive(true);
 
         maskTransform.anchoredPosition = zoomOutTransform.anchoredPosition;
         maskTransform.sizeDelta = zoomOutTransform.sizeDelta;
@@ -121,16 +134,52 @@ public class Minimap : MonoBehaviour
         {
             this.transform.localScale = Vector3.one * zoom;
 
-            slimePos = slimeObj.transform.position;
-            slimePos.y = slimePos.z;
-            slimePos.z = 0;
+           slimePos = IsOutRange(WorldPositionTomapPostion(slimeObj.transform.position));
 
             this.transform.localPosition = -slimePos * mul * zoom;
+
+            if (!isOutRangeX && !isOutRangeY)
+            {
+                slimeIconZoomIn.SetActive(true);
+                slimeIconZoomOut.SetActive(false);
+
+                slimeIconRect.anchoredPosition = Vector2.zero;
+            } 
         }
         else
         {
             this.transform.localScale = Vector3.one;
         } 
+    }
+
+    Vector2 IsOutRange(Vector2 pos)
+    {
+        // 아이콘이 범위를 벗어났을 경우
+        if (pos.x < zoomInRange * -1)
+        {
+            pos.x = zoomInRange * -1.01f;
+            isOutRangeX = true;
+        }
+        else if (pos.x > zoomInRange)
+        {
+            pos.x = zoomInRange * 1.01f;
+            isOutRangeX = true;
+        }
+        else isOutRangeX = false;
+
+        if (pos.y < zoomInRange * -1)
+        {
+            pos.y = zoomInRange * -1.01f;
+            isOutRangeY = true;
+        }
+        else if (pos.y > zoomInRange)
+        {
+            pos.y = zoomInRange * 1.01f;
+            isOutRangeY = true;
+        }
+        else isOutRangeY = false;
+
+        return pos;
     }
 
     // 아이콘의 위치 바꿈
@@ -149,12 +198,32 @@ public class Minimap : MonoBehaviour
                     iconPosition = WorldPositionTomapPostion(minimapWorldObject.transform.position);
                     minimapIcon.rectTransform.anchoredPosition = iconPosition * mul;
                 }
+                else
+                {
+                    if (isOutRangeX || isOutRangeY)
+                    {
+                        slimeIconZoomIn.SetActive(false);
+                        slimeIconZoomOut.SetActive(true);
+
+                        minimapIcon = kvp.Value;
+
+                        iconPosition = WorldPositionTomapPostion(minimapWorldObject.transform.position);
+                        minimapIcon.rectTransform.anchoredPosition = iconPosition * mul;
+                    }
+                }
             }
             else
             {
                 minimapIcon = kvp.Value;
-
                 iconPosition = WorldPositionTomapPostion(minimapWorldObject.transform.position);
+
+                // 아이콘이 범위를 벗어나지 못하도록
+                if (iconPosition.x < zoomOutRange * -1) iconPosition.x = zoomOutRange * -1;
+                else if (iconPosition.x > zoomOutRange) iconPosition.x = zoomOutRange;
+
+                if (iconPosition.y < zoomOutRange * -1) iconPosition.y = zoomOutRange * -1;
+                else if (iconPosition.y > zoomOutRange) iconPosition.y = zoomOutRange;
+
                 minimapIcon.rectTransform.anchoredPosition = iconPosition * mul;
             }
         }
@@ -172,14 +241,18 @@ public class Minimap : MonoBehaviour
     public void RegisterMinimapWorldObject(MinimapWorldObject obj)
     {
         //MinimapIcon miniMapIcon = Instantiate(miniMapIconPrefab);
-        //newIcon = ObjectPoolingManager.Instance.Get(EObjectFlag.minimapIcon).GetComponent<MinimapIcon>();
-        newIcon = Instantiate(miniMapIconPrefab);
+        newIcon = ObjectPoolingManager.Instance.Get(EObjectFlag.minimapIcon).GetComponent<MinimapIcon>();
+        //newIcon = Instantiate(miniMapIconPrefab);
         newIcon.transform.SetParent(this.transform);
         newIcon.SetIcon(obj.Icon);
         newIcon.SetColor(obj.IconColor);
         miniMapWorldObjectLookup[obj] = newIcon;
 
-        if (obj.CompareTag("Slime")) slimeObj = obj;
+        if (obj.CompareTag("Slime"))
+        {
+            slimeObj = obj;
+            slimeIconZoomOut = newIcon.gameObject;
+        }
     }
     #endregion
 }
