f8c0600666baab20c43f6ddbd9af3cee9561d0ad
woody0108 <108109854+woody0108@users.noreply.github.com>
2022-08-19 17:48:43 +0900

    Last ADD

--- SLIME_in_the_rain/Assets/Jeon/Inventory/Scripts/RanWeap.cs
+++ SLIME_in_the_rain/Assets/Jeon/Inventory/Scripts/RanWeap.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class RanWeap : MonoBehaviour
 {
     public GameObject[] pos = new GameObject[3];
-
-    
+    [SerializeField]
+    private GameObject[] debugPos = new GameObject[5];
 
     private void Start()
     {
         for (int i = 0; i < pos.Length; i++)
         {
-           ItemDatabase.Instance.weaponDrop(pos[i].transform.position + (Vector3.up * 0.5f));
+            ItemDatabase.Instance.weaponDrop(pos[i].transform.position + (Vector3.up * 0.5f));
+        }
+
+        for (int i = 0; i < debugPos.Length; i++)
+        {
+            GameObject go = ObjectPoolingManager.Instance.GetFieldItem(ItemDatabase.Instance.AllitemDB[i + 15], debugPos[i].transform.position + Vector3.up * 0.3f);
+            go.layer = go.transform.GetChild(0).gameObject.layer;
         }
 
     }
