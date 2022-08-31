5c5e4e727c43de4c088b93c9a0f1e67a38f6ec17
MallangBag <108123502+MallangBag@users.noreply.github.com>
2022-08-31 14:34:44 +0900

    Update Village/Outline
    - 울타리가 좀 보이게 아웃라인 항상 켜져있게 함

--- SLIME_in_the_rain/Assets/Lee/Scripts/Village/TowerCollider.cs
+++ SLIME_in_the_rain/Assets/Lee/Scripts/Village/TowerCollider.cs
@@ -7,15 +7,19 @@ public class TowerCollider : MonoBehaviour
     public static bool onStay;
     public static GameObject thisObject;
 
+    private void Start()
+    {
+        this.transform.GetComponent<Outline>().OutlineColor = new Color(1, 0, 0 , 0.1f);
+    }
+
     #region 콜라이더 함수
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.tag == "Slime")
         {
-            
             onStay = true;
             thisObject = this.gameObject;
-            this.transform.GetComponent<Outline>().enabled = true;
+            this.transform.GetComponent<Outline>().OutlineColor = new Color(1f, 0, 0, 1f);
         }
     }
 
@@ -25,7 +29,7 @@ public class TowerCollider : MonoBehaviour
         {
             onStay = false;
             thisObject = null;
-            this.transform.GetComponent<Outline>().enabled = false;
+            this.transform.GetComponent<Outline>().OutlineColor = new Color(1f, 0, 0, 0.1f);
         }
     }
     #endregion
